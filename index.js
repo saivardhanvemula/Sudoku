8056e68fad381e5e480a69e40887080ea7dbe1da
saivardhanvemula <saivardhanvemulamncl@gmail.com>
2024-01-09 17:10:24 +0530

    Sudoku

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-document.querySelectorAll(".num").forEach((box)=>{
-    box.addEventListener("click",()=>{
-        console.log(box.innerHTML)
+let selected=0
+document.querySelectorAll(".num").forEach((b)=>{
+    b.addEventListener("click",()=>{
+        selected=Number(b.innerHTML)
+        // b.classList.add("select")
     });
 });
