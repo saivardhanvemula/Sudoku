7f8ebf1951cceb225a706d1c6c1277d7a03cefe5
saivardhanvemula <saivardhanvemulamncl@gmail.com>
2024-01-13 22:39:54 +0530

    layout

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-let selected=0
-document.querySelectorAll(".num").forEach((b)=>{
-    b.addEventListener("click",()=>{
-        selected=Number(b.innerHTML)
-        // b.classList.add("select")
+let selected = 0;
+nums = document.querySelectorAll(".num");
+nums.forEach((b) => {
+    b.addEventListener("click", () => {
+        // selected = Number(b.innerHTML);
+        selected = b;
+        selected.classList.add("select");
+
+        nums.forEach((a) => {
+            if (a !== selected) {
+                a.classList.remove("select");
+            }
+        });
+    });
+});
+document.querySelectorAll(".box").forEach((b) => {
+    b.addEventListener("click", () => {
+        if (selected) {
+            b.innerHTML = selected.innerHTML;
+        }
     });
 });
